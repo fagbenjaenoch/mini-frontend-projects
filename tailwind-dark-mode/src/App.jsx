274968d3b5c0eb5c0aaa218e8594dc3c3e03dd51
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2024-04-20 19:51:56 +0100

    Added lucide icons and finalize landing page

--- tailwind-dark-mode/src/App.jsx
+++ tailwind-dark-mode/src/App.jsx
@@ -1,13 +1,33 @@
-import './App.css'
-import Nav from './components/organisms/Nav'
+import { ArrowDown } from "lucide-react";
+import "./App.css";
+import Container from "./components/atoms/Container";
+import Nav from "./components/organisms/Nav";
+import { ArrowDownIcon } from "lucide-react";
 
 function App() {
-  return (
-    <div className='font-clashDisplay'>
-      <Nav />
-      <h1 className="text-5xl font-bold">Hey there</h1>
-    </div>
-  )
+	return (
+		<div className="font-clashDisplay h-screen relative">
+			<Nav />
+			<Container>
+				<section className="mt-14">
+					<div>
+						<h1 className="text-7xl font-bold tracking-tight">
+							Design Systems,
+							<br /> A Book By Alexander Schvets
+						</h1>
+						<div className="mt-4 flex gap-6">
+							<p>Enoch Fagbenja</p>
+							<p>20 April, 2024</p>
+						</div>
+					</div>
+				</section>
+			</Container>
+			<ArrowDownIcon
+				className="absolute bottom-0 left-4 animate-bounce"
+				size={30}
+			/>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- tailwind-dark-mode/src/components/organisms/Nav.jsx
+++ tailwind-dark-mode/src/components/organisms/Nav.jsx
@@ -1,14 +1,17 @@
+import Container from "../atoms/Container";
 import DesktopNav from "../molecules/DesktopNav";
 
 export default function Nav() {
-  return (
-    <div className="mb-4">
-      <div className="hidden sm:block">
-        <DesktopNav />
-      </div>
-      <div className="block sm:hidden">
-        <DesktopNav />
-      </div>
-    </div>
-  );
+	return (
+		<div className="mb-4">
+			<div className="hidden sm:block py-6">
+				<Container>
+					<DesktopNav />
+				</Container>
+			</div>
+			<div className="block sm:hidden">
+				<DesktopNav />
+			</div>
+		</div>
+	);
 }
