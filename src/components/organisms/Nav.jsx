bae310f14122171f6154211b9f6ceac71a4eba5a
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2024-09-01 10:00:55 +0100

    refactor navbar responsiveness

--- src/components/organisms/Nav.jsx
+++ src/components/organisms/Nav.jsx
@@ -4,12 +4,7 @@ import DesktopNav from "../molecules/DesktopNav";
 export default function Nav() {
 	return (
 		<div className="mb-4">
-			<Container>
-				<div className="hidden sm:block py-6">
-					<DesktopNav />
-				</div>
-			</Container>
-			<div className="block sm:hidden px-4 py-6">
+			<div className="block px-4 py-6">
 				<DesktopNav />
 			</div>
 		</div>
